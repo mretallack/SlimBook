d34e65008fe3edce0652c64ee2206846046e872a
mark <mark@retallack.org.uk>
2026-07-29 15:57:54 +0100

    Fix: Disable dependency metadata in APK for F-Droid reproducible builds

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,6 +31,11 @@ android {
         }
     }
 
+    dependenciesInfo {
+        includeInApk = false
+        includeInBundle = false
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
